3e49fe580c335652ad157e7d5e6142ed91efdc9c
Alex Wilson <fonzmp@gmail.com>
2018-05-15 16:59:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,6 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |name, index|
     output << "#{index + 1}. #{name}"
-    position = index + 1
-    puts position + ". #{name}"
   end
 end
 
