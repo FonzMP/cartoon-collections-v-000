525e9a6be3cfe02520a31ed04bf92144a0b96e85
Alex Wilson <fonzmp@gmail.com>
2018-05-15 17:05:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,10 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)# code an argument here
-  # Your code here
+  # Your code here'
+  holder_array = []
+  array.each do |item|
+    
 end
 
 def long_planeteer_calls# code an argument here
