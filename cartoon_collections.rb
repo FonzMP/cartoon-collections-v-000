142a990c708b237c45b83184590af439a97f849d
Alex Wilson <fonzmp@gmail.com>
2018-05-15 17:10:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here'
   holder_array = []
-  array.map do |item|
+  array.each do |item|
     capital = item.capitalize
     holder_array << "#{capital}!"
   end
