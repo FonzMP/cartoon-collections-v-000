cb26ed6e20035498b5878f9eaea342e50dc4f872
Alex Wilson <fonzmp@gmail.com>
2018-05-15 16:50:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,6 +3,8 @@ def roll_call_dwarves(array)# code an argument here
   i = 0
   while i < array.length
     yield(array[i])
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
