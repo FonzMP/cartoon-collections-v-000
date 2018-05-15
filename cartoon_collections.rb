ebac112ef6764c4481de328499b6e87de25dfed9
Alex Wilson <fonzmp@gmail.com>
2018-05-15 16:53:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index(name, index)
   list_number = index + 1
-  puts "#{index}"
+  puts list_number
 end
 
 def summon_captain_planet# code an argument here
