fcc855e025f2023654f79484d6ddd4debcda81e0
Alex Wilson <fonzmp@gmail.com>
2018-05-15 17:23:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls(array)# code an argument here
     length = word.length
     if length > 4
       holder << word
-      puts word
+      puts holder
     else
       false
     end
